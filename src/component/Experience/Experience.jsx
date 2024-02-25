13a71ba45c8d15cd465ade407873f1370831a1f3
irfan <irfan>
2024-02-25 14:00:21 +0500

    the bugs removed from contact and experience section

--- src/component/Experience/Experience.jsx
+++ src/component/Experience/Experience.jsx
@@ -12,31 +12,31 @@ function Experience() {
         My Experience
       </h1>
       <div className=" h-[80%] w-[90%] flex justify-center items-center gap-6">
-        <div className="w-full h-full  rounded-[20px] bg-colorbgvariant flex justify-center flex-col items-center gap-10 hover:bg-colorbg border  border-transparent  hover:border hover:border-gray-600 duration-500">
+        <div className="w-full h-full  rounded-[20px] bg-colorbgvariant flex justify-center flex-col items-center gap-10 hover:bg-colorbg border  border-transparent  hover:border hover:border-gray-600 duration-500 sm:text-left text-center">
           <div className="flex justify-around w-full h-[22%]">
             <div className="w-[20%] flex flex-col justify-center items-center">
               <img src="../../../public/Skill/html-5-svgrepo-com.svg" />
-              <p className="font-semibold md:text-[18px] text-[14px]">HTML</p>
+              <p className="font-semibold md:text-[18px] text-[10px]">HTML</p>
             </div>
             <div className="w-[20%] flex flex-col justify-center items-center">
               <img src="../../../public/Skill/css-5-svgrepo-com.svg" />
-              <p className="font-semibold md:text-[18px] text-[14px]">CSS</p>
+              <p className="font-semibold md:text-[18px] text-[10px]">CSS</p>
             </div>
             <div className="w-[20%] flex flex-col justify-center items-center">
               <img src="../../../public/Skill/bootstrap-fill-svgrepo-com.svg" />
-              <p className="font-semibold md:text-[18px] text-[14px]">
+              <p className="font-semibold md:text-[18px] text-[10px]">
                 Bootstrap
               </p>
             </div>
             <div className="w-[20%] flex flex-col justify-center items-center">
               <img src="../../../public/Skill/tailwindcss-icon-svgrepo-com.svg" />
-              <p className="font-semibold md:text-[18px] text-[14px]">
+              <p className="font-semibold md:text-[18px] text-[10px]">
                 Tailwind
               </p>
             </div>
             {/* <div className="w-[20%] flex flex-col justify-center items-center gap-4">
               <p className="text-[500%]  h-[100%] mt-[30px]  ">💅</p>
-              <p className="font-semibold md:text-[18px] text-[14px] mt-[40px]">
+              <p className="font-semibold md:text-[18px] text-[10px] mt-[40px]">
                 Styled Component
               </p>
             </div> */}
@@ -44,51 +44,54 @@ function Experience() {
           <div className="flex justify-around w-full h-[22%]">
             <div className="w-[20%] flex flex-col justify-center items-center">
               <img src="../../../public/Skill/javascript-svgrepo-com.svg" />
-              <p className="font-semibold md:text-[18px] text-[14px]">
+              <p className="font-semibold md:text-[18px] text-[10px]">
                 JavaScript
               </p>
             </div>
             <div className="w-[20%] flex flex-col justify-center items-center">
               <img src="../../../public/Skill/react-svgrepo-com.svg" />
-              <p className="font-semibold md:text-[18px] text-[14px]">React</p>
+              <p className="font-semibold md:text-[18px] text-[10px]">React</p>
             </div>
             <div className="w-[20%] flex flex-col justify-center items-center">
               <img src="../../../public/Skill/redux-logo-svgrepo-com.svg" />
-              <p className="font-semibold md:text-[18px] text-[14px]">Redux</p>
+              <p className="font-semibold md:text-[18px] text-[10px]">Redux</p>
             </div>
             <div className="w-[20%] flex flex-col justify-center items-center">
               <img src="../../../public/Skill/chakraui-svgrepo-com (1).svg" />
-              <p className="font-semibold md:text-[18px] text-[14px]">
+              <p className="font-semibold md:text-[18px] text-[10px]">
                 Chakra Ui
               </p>
             </div>
             {/* <div className="w-[20%] flex flex-col justify-center items-center">
               <img src="../../../public/Skill/git-svgrepo-com.svg" />
-              <p className="font-semibold md:text-[18px] text-[14px]">Git</p>
+              <p className="font-semibold md:text-[18px] text-[10px]">Git</p>
             </div> */}
           </div>
           <div className="flex justify-around w-full h-[22%]">
             <div className="w-[20%] flex flex-col justify-center items-center">
               <img src="../../../public/Skill/nodejs-icon-svgrepo-com.svg" />
-              <p className="font-semibold md:text-[18px] text-[14px]">
+              <p className="font-semibold md:text-[18px] text-[10px]">
                 Node JS
               </p>
             </div>
             <div className="w-[20%] flex flex-col justify-center items-center">
               <img src="../../../public/Skill/express-js-icon.svg" />
-              <p className="font-semibold md:text-[18px] text-[14px]">
+              <p className="font-semibold md:text-[18px] text-[10px]">
                 Express JS
               </p>
             </div>
             <div className="w-[20%] flex flex-col justify-center items-center">
               <img src="../../../public/Skill/mongo-svgrepo-com.svg" />
-              <p className="font-semibold md:text-[18px] text-[14px]">
+              <p className="font-semibold md:text-[18px] text-[10px]">
                 MongoDB
               </p>
             </div>
             <div className="w-[20%] flex flex-col justify-center items-center">
-              <img src="../../../public/Skill/Daco_2749343.png" />
-              <p className="font-semibold md:text-[18px] text-[14px]">
+              <img
+                src="../../../public/Skill/Daco_2749343.png"
+                className="object-contain"
+              />
+              <p className="font-semibold md:text-[18px] text-[10px]">
                 Mongoose
               </p>
             </div>
--- src/component/contact/Contact.jsx
+++ src/component/contact/Contact.jsx
@@ -7,20 +7,20 @@ import { FaFacebookMessenger } from "react-icons/fa";
 function Contact() {
   return (
     <section
-      className="h-lvh  flex justify-center flex-col sm:w-[90%] md:w-[86%] lg:w-[75%] mt-3 items-center  mx-auto  gap-4 border-2  "
+      className="sm:h-lvh  flex justify-center flex-col sm:w-[90%] md:w-[86%] lg:w-[75%] mt-3 items-center  mx-auto  gap-4 border-2  "
       id="experience"
     >
       <h5 className="text-[#fff4f4b7] font-medium">My Recent Work</h5>
       <h1 className="text-[35px] font-medium text-colorprimary mb-3">
         Portfolio
       </h1>
-      <div className="w-full h-[90%] border-2 border-red-600 flex p-4 gap-6">
-        <div className="w-1/2 h-full border-2 border-green-600 flex flex-col items-center gap-2 justify-center ">
+      <div className="w-full h-[90%]  border-2 border-red-600 flex flex-col sm:flex-row p-4 gap-6">
+        <div className="sm:w-1/2 w-full sm:h-full  h-[50%] border-2 border-green-600 flex flex-col items-center gap-2 justify-center ">
           <a
             href="#"
-            className="text-white w-1/2 h-[30%] flex flex-col justify-center items-center   "
+            className="text-white xl:w-1/2 w-[80%] h-[30%] flex flex-col justify-center items-center   "
           >
-            <div className=" rounded-xl w-full h-full  gap-1 bg-colorbgvariant hover:bg-colorbg border border-transparent  hover:border hover:border-gray-600 duration-500 flex flex-col items-center">
+            <div className=" rounded-xl w-full h-full  gap-[2px] bg-colorbgvariant hover:bg-colorbg border border-transparent  hover:border hover:border-gray-600 duration-500 flex flex-col items-center">
               <MdMailOutline className="text-[40px]" />
               <p className="font-bold text-[18px]">Email</p>
               <p className="font-bold text-[14px]">khange221133@gmail.com</p>
@@ -29,9 +29,9 @@ function Contact() {
           </a>
           <a
             href="#"
-            className="text-white w-1/2 h-[30%] flex flex-col justify-center items-center"
+            className="text-white xl:w-1/2 w-[80%] h-[30%] flex flex-col justify-center items-center"
           >
-            <div className=" rounded-xl w-full h-full flex flex-col justify-center items-center gap-1 bg-colorbgvariant hover:bg-colorbg border border-transparent  hover:border hover:border-gray-600 duration-500">
+            <div className=" rounded-xl w-full h-full flex flex-col justify-center items-center gap-[2px] bg-colorbgvariant hover:bg-colorbg border border-transparent  hover:border hover:border-gray-600 duration-500">
               <FaFacebookMessenger className="text-[40px]" />
               <p className="font-bold text-[18px]">Messanger</p>
               <p className="font-bold text-[14px]">IrfanUllah</p>
@@ -40,9 +40,9 @@ function Contact() {
           </a>
           <a
             href="#"
-            className=" text-white w-1/2 h-[30%] flex flex-col justify-center items-center "
+            className=" text-white xl:w-1/2 w-[80%] h-[30%] flex flex-col justify-center items-center "
           >
-            <div className=" rounded-xl w-full h-full flex flex-col justify-center items-center gap-1 bg-colorbgvariant hover:bg-colorbg border border-transparent  hover:border hover:border-gray-600 duration-500">
+            <div className=" rounded-xl w-full h-full flex flex-col justify-center items-center gap-[2px] bg-colorbgvariant hover:bg-colorbg border border-transparent  hover:border hover:border-gray-600 duration-500">
               <BsWhatsapp className="text-[40px]" />
               <p className="font-bold text-[18px]">WhatsApp</p>
               <p className="font-bold text-[14px]">khange221133@gmail.com</p>
@@ -50,7 +50,41 @@ function Contact() {
             </div>
           </a>
         </div>
-        <div className="w-1/2 h-full border-2 border-green-600"></div>
+        <div className="sm:w-1/2 w-full sm:h-full  h-[50%] border-2 border-green-600 flex flex-col items-center gap-2 justify-center ">
+          <a
+            href="#"
+            className="text-white xl:w-1/2 w-[80%] h-[30%] flex flex-col justify-center items-center   "
+          >
+            <div className=" rounded-xl w-full h-full  gap-[2px] bg-colorbgvariant hover:bg-colorbg border border-transparent  hover:border hover:border-gray-600 duration-500 flex flex-col items-center">
+              <MdMailOutline className="text-[40px]" />
+              <p className="font-bold text-[18px]">Email</p>
+              <p className="font-bold text-[14px]">khange221133@gmail.com</p>
+              <p className="text-colorprimary">Send a message</p>
+            </div>
+          </a>
+          <a
+            href="#"
+            className="text-white xl:w-1/2 w-[80%] h-[30%] flex flex-col justify-center items-center"
+          >
+            <div className=" rounded-xl w-full h-full flex flex-col justify-center items-center gap-[2px] bg-colorbgvariant hover:bg-colorbg border border-transparent  hover:border hover:border-gray-600 duration-500">
+              <FaFacebookMessenger className="text-[40px]" />
+              <p className="font-bold text-[18px]">Messanger</p>
+              <p className="font-bold text-[14px]">IrfanUllah</p>
+              <p className="text-colorprimary">Send a message</p>
+            </div>
+          </a>
+          <a
+            href="#"
+            className=" text-white xl:w-1/2 w-[80%] h-[30%] flex flex-col justify-center items-center "
+          >
+            <div className=" rounded-xl w-full h-full flex flex-col justify-center items-center gap-[2px] bg-colorbgvariant hover:bg-colorbg border border-transparent  hover:border hover:border-gray-600 duration-500">
+              <BsWhatsapp className="text-[40px]" />
+              <p className="font-bold text-[18px]">WhatsApp</p>
+              <p className="font-bold text-[14px]">khange221133@gmail.com</p>
+              <p className="text-colorprimary">Send a message</p>
+            </div>
+          </a>
+        </div>
       </div>
     </section>
   );
